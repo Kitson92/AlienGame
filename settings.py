240c136bc8ddc3b3673c1893ca0b0b82899bda6b
alex kitson <alexkitson92@hotmail.com>
2021-09-24 10:23:41 +0100

    fixing bug in ship class and implementing ship class into alien_invasion

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ class settings:
         """Initialize the game's settings"""
         #screen settings
         self.screen_width = 1200
-        self.screen_height = 900
+        self.screen_height = 800
         self.bg_colour = (230,230,230)
