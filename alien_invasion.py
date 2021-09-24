a9afe8cacad8c8e28899552d46d7b20702f79773
Kitson92 <72226088+Kitson92@users.noreply.github.com>
2021-09-24 10:09:53 +0100

    Creating settings (#1)
    * Fixed a bug in the code when executing the game
    
    * adding background colour
    
    * adding settings file to create a settings class in order to set screen properties

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,26 +1,37 @@
 import sys
+
 import pygame
 
-class AlienInvasion:
-    """Over class to manage game assets and behaviour"""
-    def __init__(self):
-        """Initialize the game, and create game resource"""
-        pygame.init()
+from settings import settings
 
-        self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien Invasion")
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Iitialize the game, and create game resources"""
+        pygame.init()
+        self.settings = settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")    
+
+        #Set the background colour.
+        self.bg_colour = (230, 230, 230)
 
     def run_game(self):
-        """Start the main loop for the game"""
+        """Start the main loop for the game."""
         while True:
             #Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            #Redraw screen during each pass through the loop
+            self.screen.fill(self.settings.bg_colour)
+            """Make the most recently drawn screen visible"""
             pygame.display.flip()
 
-if __name__ == '__Main__':
-    #Make a game instance, and run the game.
+if __name__ == '__main__':
+    # Make a game instance, and run the game
     ai = AlienInvasion()
-    ai.run_game
+    ai.run_game()
