25a936c214ef25d3b7dca273cbbeb2e127c690d7
Kitson92 <72226088+Kitson92@users.noreply.github.com>
2021-09-24 17:41:55 +0100

    added music loop and added a game version number (#6)

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 import pygame
+from pygame import mixer
+
+
 
 class settings:
     """A clas to store all the settings for Alien Invasion"""
@@ -9,6 +12,18 @@ class settings:
         self.screen_height = 1000
         self.bg_colour = (230,230,230)
         self.background_image = pygame.image.load('images/background.png')
-
+        self.font = pygame.font.Font('freesansbold.ttf',32)
+        self.game_title = self.font.render('Alien Invasion Version 1',True,(240,20,100))
+        
+        
         #ship settings
-        self.ship_speed = 4
+        self.ship_speed = 1.5
+
+    def music_play():
+            mixer.init()
+            mixer.music.load('music/GameMusicLoop.ogg') 
+            mixer.music.play(loops = -1)
+    
+
+
+    
