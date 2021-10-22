a208ef65d29885e618ffea939fdd6f277663e973
Kitson92 <72226088+Kitson92@users.noreply.github.com>
2021-10-22 08:44:36 +0100

    adding bullets to the game (#7)

--- settings.py
+++ settings.py
@@ -10,14 +10,20 @@ class settings:
         #screen settings
         self.screen_width = 1200
         self.screen_height = 1000
-        self.bg_colour = (230,230,230)
         self.background_image = pygame.image.load('images/background.png')
+        
+        #Title of game
         self.font = pygame.font.Font('freesansbold.ttf',32)
         self.game_title = self.font.render('Alien Invasion Version 1',True,(240,20,100))
         
-        
         #ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 2
+
+        #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (255,40,0)
 
     def music_play():
             mixer.init()
